3e91b7aad159908b6a7adde073007ccd58b7da09
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-05-06 00:34:45 +0200

    Keyboard behaviour

--- app/src/Keyboard.js
+++ app/src/Keyboard.js
@@ -2,51 +2,79 @@ import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
 
 class Keyboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {number: ''};
+  }
+
+  buttonClicked(num) {
+    this.setState((prevState) => {
+      return {number: prevState.number.concat(num)};
+    });
+  }
+
+  addClicked() {
+    this.setState(() => {
+      return {number: ''};
+    });
+  }
+
+  deleteClicked() {
+    this.setState((prevState) => {
+      return {number: prevState.number.slice(0, -1)};
+    });
+  }
+
   render(){
     return (
       <Container>
         <Row>
           <Col>
-            <Button block="true">1</Button>
-          </Col>
-          <Col>
-            <Button block="true">2</Button>
-          </Col>
-          <Col>
-            <Button block="true">3</Button>
+            <h1 className="text-center">{this.state.number || '-'}</h1>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button block="true">4</Button>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 1)}>1</Button>
           </Col>
           <Col>
-            <Button block="true">5</Button>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 2)}>2</Button>
           </Col>
           <Col>
-            <Button block="true">6</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button block="true">7</Button>
-          </Col>
-          <Col>
-            <Button block="true">8</Button>
-          </Col>
-          <Col>
-            <Button block="true">9</Button>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 3)}>3</Button>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button block="true" color="danger">Delete</Button>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 4)}>4</Button>
           </Col>
           <Col>
-            <Button block="true">0</Button>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 5)}>5</Button>
           </Col>
           <Col>
-            <Button block="true" color="success">Add</Button>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 6)}>6</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 7)}>7</Button>
+          </Col>
+          <Col>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 8)}>8</Button>
+          </Col>
+          <Col>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 9)}>9</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button block={true} color="danger" onClick={this.deleteClicked.bind(this)}>Delete</Button>
+          </Col>
+          <Col>
+            <Button block={true} onClick={this.buttonClicked.bind(this, 0)}>0</Button>
+          </Col>
+          <Col>
+            <Button block={true} color="success" onClick={this.addClicked.bind(this)}>Add</Button>
           </Col>
         </Row>
       </Container>

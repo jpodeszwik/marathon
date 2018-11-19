030508201019876d3f549c91e5dd35073b5f0301
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-11-19 22:49:55 +0100

    possiblity to remove numbers

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import moment from 'moment';
 import './App.css';
 import AppHeader from './AppHeader';
 import Keyboard from './Keyboard';
 import RoundPicker from './RoundPicker';
 import NumberList from './NumberList.jsx';
-import firebase from '../services/firebase';
-
-const round = date => moment(date).format('DD MMM HH:mm');
+import { pushNumber, readNumbers, removeNumber } from '../services/numbers';
 
 class App extends Component {
   constructor() {
     super();
-    this.round = round(new Date());
+    this.round = new Date();
     this.state = { numbers: [] };
     this.numberSent = this.numberSent.bind(this);
     this.roundSelected = this.roundSelected.bind(this);
@@ -21,7 +18,7 @@ class App extends Component {
   }
 
   numberSent(number) {
-    firebase.database().ref(this.round).push(number);
+    pushNumber(this.round, number);
     this.setState((prevState) => {
       const newNumbers = prevState.numbers.slice();
       newNumbers.unshift(number);
@@ -32,11 +29,14 @@ class App extends Component {
   }
 
   roundSelected(date) {
-    this.round = round(date);
-    this.setState({ numbers: [] });
+    this.round = date;
+    readNumbers(date).then((numbers) => {
+      this.setState({ numbers });
+    });
   }
 
   numberRemoved(number) {
+    removeNumber(this.round, number);
     this.setState((prevState) => {
       const newNumbers = prevState.numbers.slice()
         .filter(val => number !== val);
---
+++ app/src/services/numbers.js
@@ -0,0 +1,28 @@
+import moment from 'moment';
+import firebase from './firebase';
+
+const format = date => moment(date).format('DD MMM HH:mm');
+
+const roundRef = round => firebase.database().ref(format(round));
+
+const numberRef = (round, number) => roundRef(round).child(number);
+
+const pushNumber = (round, number) => {
+  numberRef(round, number).set(1);
+};
+
+const removeNumber = (round, number) => {
+  numberRef(round, number).set(0);
+};
+
+const readNumbers = round =>
+  roundRef(round).once('value').then((snapshot) => {
+    const val = snapshot.val();
+    if (val === null) {
+      return [];
+    }
+    return Object.keys(val)
+      .filter(key => val[key] === 1);
+  });
+
+export { pushNumber, removeNumber, readNumbers };

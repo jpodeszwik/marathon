faebaa3b4aca9131a150271337ddbf561e23f363
TomaszMoroz <tmoroz688@gmail.com>
2018-12-16 15:16:59 +0100

    form upgrade

--- src/components/stats.js
+++ src/components/stats.js
@@ -26,17 +26,14 @@ class Stats extends Component{
         return(
         
             <div>
+
             <form onSubmit={this.handleSubmit}>
-                <input id="input" value={this.state.userId} onChange={this.handleChange}  />
+                <input id="input" value={this.state.userId} placeholder="podaj nr zawodnika" onChange={this.handleChange}  />
             </form>
             
-            {<h5>Zawodnik z numerem {this.state.userNumber} stoczyl {this.props.state.filter(item => item.content[this.state.userNumber] === 1).length} walk</h5>}
+            {<h5>Zawodnik z numerem {this.state.userNumber === "" ? "X" : this.state.userNumber} stoczyl {this.props.state.filter(item => item.content[this.state.userNumber] === 1).length} walk</h5>}
             <ul className="fightsList">
-                {/* {this.props.msg} */}
-        
-             
-               
-              {<li>{<ol>{this.props.state.filter(item => item.content[this.state.userNumber] === 1).map( i => <li key={i.key} className="dates">{i.key}</li>)}</ol>}</li>} 
+                {<li>{<ol>{this.props.state.filter(item => item.content[this.state.userNumber] === 1).map( i => <li key={i.key} className="dates">{i.key}</li>)}</ol>}</li>} 
             </ul>  
     
            </div>

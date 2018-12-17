f0fa27f11b6b60169c70f04b72728e4aba4db7dc
TomaszMoroz <tmoroz688@gmail.com>
2018-12-17 18:47:46 +0100

    css tran-group added

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Stats.css';
 import { Table, FormControl, Grid, Row, Col } from 'react-bootstrap';
+import { CSSTransitionGroup } from 'react-transition-group'
  
 class Stats extends Component{
     constructor(props){
@@ -26,49 +27,57 @@ class Stats extends Component{
     render(){
         return(
         
-            <Grid>
+        //    <CSSTransitionGroup 
+        //    transitionName="example"
+        //    transitionAppear={true}
+        //    transitionAppearTimeout={500}
+        //    transitionEnter={false}
+        //    transitionLeave={false}>
+                <Grid>
 
-                <Row className="nav-row">
-                    <Col sm={10} smOffset={1}>
-                        <form onSubmit={this.handleSubmit}>
-                        <FormControl 
-                            value={this.state.userId}
-                            placeholder="podaj nr zawodnika"
-                            onChange={this.handleChange}
+                    <Row className="nav-row">
+                        <Col sm={10} smOffset={1}>
+                             <form onSubmit={this.handleSubmit}>
+                                <FormControl 
+                                    value={this.state.userId}
+                                    placeholder="podaj nr zawodnika"
+                                    onChange={this.handleChange}
+                                />
+                            </form>
+                        </Col>
+                    </Row>
 
-                        />
-                        </form>
-                    </Col>
-                </Row>
-            
-                <Row className="nav-row">
-                    <Col sm={10} smOffset={1}>
-                    
-                        {<h5 className="info-header">Zawodnik z numerem {this.state.userNumber === "" ? "X" : this.state.userNumber} stoczyl {this.props.state.filter(item => item.content[this.state.userNumber] === 1).length} walk</h5>}
-                      
-                    </Col>
-                </Row>
-           
-                <Row className="nav-row">
-                    <Col sm={10} smOffset={1}>
-                    
-                    <Table striped bordered condensed hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Data i godzina rundy</th>
-                    </tr>
-                    
-                    {this.props.state.filter(item => item.content[this.state.userNumber] === 1).map( (i, index) => <tr key={i.key}><td>{index + 1}</td><td>{i.key}</td></tr>)}
-                    
-                </thead>
-            </Table>
-                      
-                    </Col>
-                </Row>
-            
+                    <Row className="nav-row">
+                        <Col sm={10} smOffset={1}>
     
-           </Grid>
+                            {<h5 className="info-header">Zawodnik z numerem {this.state.userNumber === "" ? "X" : this.state.userNumber} stoczyl {this.props.state.filter(item => item.content[this.state.userNumber] === 1).length} walk</h5>}
+      
+                        </Col>
+                    </Row>
+
+                    <Row className="nav-row">
+                        <Col sm={10} smOffset={1}>
+    
+                            <Table striped bordered condensed hover>
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Data i godzina rundy</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    
+                                {this.props.state.filter(item => item.content[this.state.userNumber] === 1).map( (i, index) => <tr key={i.key}><td>{index + 1}</td><td>{i.key}</td></tr>)} 
+                                </tbody>
+                                
+                            </Table>  
+      
+                        </Col>
+                    </Row>
+
+
+                </Grid>
+        //    </CSSTransitionGroup>
         );
     }
     }

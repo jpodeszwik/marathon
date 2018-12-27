193162fd87ca5ee2649c7f5b37a3ffd8acfa6a5e
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-12-27 12:30:31 +0100

    saving firstName in separate part of the tree

--- functions/index.js
+++ functions/index.js
@@ -14,13 +14,13 @@ exports.processSignUp = functions.auth.user().onCreate(user => {
   });
 });
 
-exports.sumPersonsFight = functions.database.ref('/fights/{personId}').onWrite((change, context) => {
-  const personId = context.params.personId;
+exports.sumPersonsFight = functions.database.ref('/fights/{participantId}').onWrite((change, context) => {
+  const participantId = context.params.participantId;
   const fights = change.after.val();
   const totalFights = Object.keys(fights).filter(fight => fights[fight] === true).length;
 
-  console.log(`person: ${context.params.personId}, totalFights: ${totalFights}`);
-  return admin.database().ref(`/ranking/${personId}/totalFights`).set(totalFights);
+  console.log(`person: ${participantId}, totalFights: ${totalFights}`);
+  return admin.database().ref(`/ranking/${participantId}/totalFights`).set(totalFights);
 });
 
 function getNextParticipantId() {
@@ -29,7 +29,9 @@ function getNextParticipantId() {
 }
 
 /**
- * Copy registered users into other place in the tree to prevent unauthorized modification
+ * Copy registered users into other place in the tree to prevent unauthorized modification.
+ * Assign id to newly created participant.
+ * Write participant first name to public part of the tree.
  */
 exports.registerParticipants = functions.database.ref('/commands/register/{userId}/{participantId}').onWrite((change, context) => {
   if (change.before.exists()) {
@@ -38,14 +40,20 @@ exports.registerParticipants = functions.database.ref('/commands/register/{userI
 
   const { userId, participantId } = context.params;
   const participant = change.after.val();
-  participant.registeredBy = userId;
+  const fullName = participant.fullName || 'unknown';
+  const firstName = fullName.split(' ')[0] || 'unknown';
 
   console.log(`registering participant: ${participantId}, created by user: ${userId}`);
 
   return getNextParticipantId()
     .then(id => {
+      console.log(`assigned id: ${id} to participant: ${participantId}`);
       participant.id = id;
-      participant.firstName = participant.fullName.split(' ')[0];
-      return admin.database().ref(`/participants/${participantId}`).set(participant)
+      participant.registeredBy = userId;
+
+      return Promise.all(
+        admin.database().ref(`/participants/${participantId}`).set(participant),
+        admin.database().ref(`/ranking/${id}/firstName`).set(firstName)
+      );
     });
 });

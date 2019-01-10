82ed2bf6fa8f0e91caa21a5bf2a262ace8387ea5
TomaszMoroz <tmoroz688@gmail.com>
2019-01-10 12:02:51 +0100

    bootstrap table styling

--- src/components/General.js
+++ src/components/General.js
@@ -6,27 +6,28 @@ import { Table } from 'react-bootstrap';
 const General =(props)=>{
     let results = props.rank.sort((a, b) =>{return a.content.totalFights < b.content.totalFights ? 1 : a.content.totalFights > b.content.totalFights ? -1 : 0}).map(item => item.content.totalFights)
     console.log(results)
+    
     return(
         <div className="general-container">
             <Navbar />
 
             <div className="logo-general"></div>
             <h3 className="general-header">RANKING OGÓLNY</h3>
-            <Table striped hover>
+            <Table striped >
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>IMIĘ</th>
-                        <th>KLUB</th>
-                        <th>ILOŚĆ WALK</th>
                         <th>MIEJSCE</th>
+                        <th>IMIĘ</th>
+                        <th>NAZWA KLUBU</th>
+                        <th>ILOŚĆ WALK</th>
+                        <th>ID</th>
                     </tr>
                 </thead>
                 <tbody>
-                {props.rank.map(item => <tr key={item.key}>
-                                            <td>{item.key}</td><td>{item.content.firstName}</td>
+                {props.rank.map(item => <tr key={item.key} style={{border: 'none'}}>
+                                            <td>{results.indexOf(item.content.totalFights)+1}</td><td>{item.content.firstName}</td>
                                             <td>{item.content.homeClub}</td><td>{item.content.totalFights}</td>
-                                            <td>{results.indexOf(item.content.totalFights)+1}</td>
+                                            <td>{item.key}</td>
                                         </tr> )}
                 </tbody>
             </Table>

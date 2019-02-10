6771848e5b20e6626db4955587dfd5f218a02307
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2019-02-10 14:11:05 +0100

    refactored DrawPrize to use hooks

--- admin/src/components/prize/DrawPrize.jsx
+++ admin/src/components/prize/DrawPrize.jsx
@@ -1,42 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { listFighters } from '../../services/users';
 import { getParticipant } from '../../services/participantRepository';
 import { Button, Container, Row } from 'reactstrap';
 import RoundPicker from './RoundPicker';
 
-class DrawPrize extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { round: new Date(), fighters: 0, winner: null };
-    this.roundSelected = this.roundSelected.bind(this);
-    this.doDrawPrize = this.doDrawPrize.bind(this);
-  }
+const DrawPrize = () => {
+  const [round, setRound] = useState(new Date());
+  const [participantsCount, setParticipantsCount] = useState(0);
+  const [winner, setWinner] = useState(null);
 
-  roundSelected(date) {
+  const roundSelected = date => {
     listFighters(date).then(fighters => {
-      this.setState({ round: date, fighters: fighters.length, winner: null });
+      setParticipantsCount(fighters.length);
+      setRound(date);
+      setWinner(null);
     });
-  }
+  };
 
-  doDrawPrize() {
-    listFighters(this.state.round)
-      .then(fighters => fighters[Math.floor(Math.random() * fighters.length)])
-      .then(winnerId => getParticipant(winnerId))
-      .then(details => details && this.setState({ winner: `${details.fullName} (${details.id})` }));
-  }
+  const doDrawPrize = () => {
+    listFighters(round)
+      .then(participants => participants[Math.floor(Math.random() * participants.length)])
+      .then(winnerId => {
+        const participant = getParticipant(winnerId);
+        setWinner(`${winnerId} ${participant.fullName}`);
+      });
+  };
 
-  render() {
-    return (
-      <Container>
-        <center>
-          <RoundPicker onRoundSelected={this.roundSelected} />
-          <Row><span>Liczba zawodników: {this.state.fighters}</span></Row>
-          <Row><Button onClick={this.doDrawPrize}>Losuj</Button></Row>
-          <Row><span>Zwycięzca: {this.state.winner}</span></Row>
-        </center>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <center>
+        <RoundPicker onRoundSelected={roundSelected} />
+        <Row>
+          <span>Liczba zawodników: {participantsCount}</span>
+        </Row>
+        <Row>
+          <Button onClick={doDrawPrize}>Losuj</Button>
+        </Row>
+        <Row>
+          <span>Zwycięzca: {winner}</span>
+        </Row>
+      </center>
+    </Container>
+  );
+};
 
 export default DrawPrize;
--- admin/src/components/prize/RoundPicker.jsx
+++ admin/src/components/prize/RoundPicker.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { Row, Col, Button } from 'reactstrap';
@@ -16,7 +16,7 @@ const pickerButtonStyle = {
   paddingRight: '20px',
 };
 
-const roundToRoundLength = (date) => {
+const roundToRoundLength = date => {
   const copiedDate = new Date(date.getTime());
 
   const minutes = copiedDate.getMinutes();
@@ -32,51 +32,44 @@ const addMinutes = (date, minutes) => {
   return copiedDate;
 };
 
-class RoundPicker extends Component {
-  constructor(props) {
-    super(props);
-    this.onRoundSelected = props.onRoundSelected;
-    const date = roundToRoundLength(new Date());
-    this.state = { roundStart: date };
-  }
+const RoundPicker = props => {
+  const [roundStart, setRoundStart] = useState(roundToRoundLength(new Date()));
+  const onRoundSelected = props.onRoundSelected || function() {};
 
-  componentDidMount() {
-    if (this.onRoundSelected) this.onRoundSelected(this.state.roundStart);
-  }
+  useEffect(() => {
+    onRoundSelected(roundStart);
+  }, []);
 
-  nextRound() {
-    this.setState((prevState) => {
-      const newRoundStart = addMinutes(prevState.roundStart, roundLength);
-      if (this.onRoundSelected) this.onRoundSelected(newRoundStart);
-      return { roundStart: newRoundStart };
-    });
-  }
+  const nextRound = () => {
+    const newRoundStart = addMinutes(roundStart, roundLength);
+    setRoundStart(newRoundStart);
+    onRoundSelected(newRoundStart);
+  };
 
-  previousRound() {
-    this.setState((prevState) => {
-      const newRoundStart = addMinutes(prevState.roundStart, -roundLength);
-      if (this.onRoundSelected) this.onRoundSelected(newRoundStart);
-      return { roundStart: newRoundStart };
-    });
-  }
+  const previousRound = () => {
+    const newRoundStart = addMinutes(roundStart, -roundLength);
+    setRoundStart(newRoundStart);
+    onRoundSelected(newRoundStart);
+  };
 
-  render() {
-    const start = this.state.roundStart;
-    const end = addMinutes(this.state.roundStart, roundLength);
+  const roundEnd = addMinutes(roundStart, roundLength);
 
-    return (
-      <Row>
-        <Col style={pickerStyle}>
-          <Button onClick={this.previousRound.bind(this)}><FaAngleLeft /></Button>
-          <div style={pickerButtonStyle}>
-            <Moment format="DD MMM">{start}</Moment> <Moment format="HH:mm">{start}</Moment> - <Moment format="HH:mm">{end}</Moment>
-          </div>
-          <Button onClick={this.nextRound.bind(this)}><FaAngleRight /></Button>
-        </Col>
-      </Row>
-    );
-  }
-}
+  return (
+    <Row>
+      <Col style={pickerStyle}>
+        <Button onClick={previousRound}>
+          <FaAngleLeft />
+        </Button>
+        <div style={pickerButtonStyle}>
+          <Moment format="DD MMM">{roundStart}</Moment> <Moment format="HH:mm">{roundStart}</Moment> - <Moment format="HH:mm">{roundEnd}</Moment>
+        </div>
+        <Button onClick={nextRound}>
+          <FaAngleRight />
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
 RoundPicker.propTypes = {
   onRoundSelected: PropTypes.func,

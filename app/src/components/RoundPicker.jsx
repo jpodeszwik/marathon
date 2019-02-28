00c368ef46a3c16e72f1b346727cdbd777d466cf
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2019-02-28 22:21:52 +0100

    refactored round picker to use flexbox

--- app/src/components/RoundPicker.jsx
+++ app/src/components/RoundPicker.jsx
@@ -1,79 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
-import { Row, Col, Button } from 'reactstrap';
+import {Button} from 'reactstrap';
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
 import FaAngleRight from 'react-icons/lib/fa/angle-right';
+import './RoundPicker.css'
 
 const roundLength = 5;
-const pickerStyle = {
-  paddingBottom: '20px',
-  paddingTop: '10px',
-};
-
-const pickerButtonStyle = {
-  display: 'inline-block',
-  paddingLeft: '20px',
-  paddingRight: '20px',
-};
 
 const roundToRoundLength = date => {
-  const copiedDate = new Date(date.getTime());
+    const copiedDate = new Date(date.getTime());
 
-  const minutes = copiedDate.getMinutes();
-  copiedDate.setMinutes(minutes - (minutes % roundLength));
-  copiedDate.setSeconds(0);
+    const minutes = copiedDate.getMinutes();
+    copiedDate.setMinutes(minutes - (minutes % roundLength));
+    copiedDate.setSeconds(0);
 
-  return copiedDate;
+    return copiedDate;
 };
 
 const addMinutes = (date, minutes) => {
-  const copiedDate = new Date(date.getTime());
-  copiedDate.setMinutes(copiedDate.getMinutes() + minutes);
-  return copiedDate;
+    const copiedDate = new Date(date.getTime());
+    copiedDate.setMinutes(copiedDate.getMinutes() + minutes);
+    return copiedDate;
 };
 
 const RoundPicker = props => {
-  const [roundStart, setRoundStart] = useState(roundToRoundLength(new Date()));
-  const onRoundSelected = props.onRoundSelected || function() {};
+    const [roundStart, setRoundStart] = useState(roundToRoundLength(new Date()));
+    const onRoundSelected = props.onRoundSelected || function () {
+    };
 
-  useEffect(() => {
-    onRoundSelected(roundStart);
-  }, []);
+    useEffect(() => {
+        onRoundSelected(roundStart);
+    }, []);
 
-  const nextRound = () => {
-    const newRoundStart = addMinutes(roundStart, roundLength);
-    setRoundStart(newRoundStart);
-    onRoundSelected(newRoundStart);
-  };
+    const nextRound = () => {
+        const newRoundStart = addMinutes(roundStart, roundLength);
+        setRoundStart(newRoundStart);
+        onRoundSelected(newRoundStart);
+    };
 
-  const previousRound = () => {
-    const newRoundStart = addMinutes(roundStart, -roundLength);
-    setRoundStart(newRoundStart);
-    onRoundSelected(newRoundStart);
-  };
+    const previousRound = () => {
+        const newRoundStart = addMinutes(roundStart, -roundLength);
+        setRoundStart(newRoundStart);
+        onRoundSelected(newRoundStart);
+    };
 
-  const roundEnd = addMinutes(roundStart, roundLength);
+    const roundEnd = addMinutes(roundStart, roundLength);
 
-  return (
-    <Row>
-      <Col style={pickerStyle}>
-        <Button onClick={previousRound}>
-          <FaAngleLeft />
-        </Button>
-        <div style={pickerButtonStyle}>
-          <Moment format="DD MMM">{roundStart}</Moment> <Moment format="HH:mm">{roundStart}</Moment> - <Moment format="HH:mm">{roundEnd}</Moment>
+    return (
+        <div className={'round-picker-container'}>
+            <Button onClick={previousRound}>
+                <FaAngleLeft/>
+            </Button>
+            <div>
+                <Moment format="DD MMM">{roundStart}</Moment> <Moment format="HH:mm">{roundStart}</Moment> - <Moment
+                format="HH:mm">{roundEnd}</Moment>
+            </div>
+            <Button onClick={nextRound}>
+                <FaAngleRight/>
+            </Button>
         </div>
-        <Button onClick={nextRound}>
-          <FaAngleRight />
-        </Button>
-      </Col>
-    </Row>
-  );
+    );
 };
 
 RoundPicker.propTypes = {
-  onRoundSelected: PropTypes.func,
+    onRoundSelected: PropTypes.func,
 };
 
 export default React.memo(RoundPicker);
